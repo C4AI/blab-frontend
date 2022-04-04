2b37979600f1966cba81bb108689904aaefdfea0
rsgrava <rsgrava@hotmail.com>
2022-04-03 22:18:31 -0300

    integrated i18n into ServicesList

--- src/ServicesList.js
+++ src/ServicesList.js
@@ -1,24 +1,27 @@
+import React from "react";
+import { Trans } from "react-i18next";
+
 const ServicesList = [
   {
     name: "ChatBot",
-    title: "ChatBot",
-    text: "Um Chabot especializado em Amazônia Azul",
+    title: <Trans i18nKey="chatBotTitle">ChatBot</Trans>,
+    text: <Trans i18nKey="chatBotText">A ChatBot specialized in Blue Amazon</Trans>,
     active: true,
     image: "Chatbot.jpg",
     imageAlt: "Ilustração de um robô de conversa",
   },
   {
     name: "Reporter",
-    title: "Robô jornalista",
-    text: "Um robô que gera notícias sobre a Amazônia Azul",
+    title: <Trans i18nKey="reporterTitle">Journalist Robot</Trans>,
+    text: <Trans i18nKey="reporterText">A robot that generates news on the Blue Amazon</Trans>,
     active: true,
     image: "RoboJornalista.jpeg",
     imageAlt: "Ilustração de um robô jornalista",
   },
   {
     name: "Wiki",
-    title: "Wiki",
-    text: "Uma wiki especializada em temas da Amazônia Azul",
+    title: <Trans i18nKey="wikiTitle">Wiki</Trans>,
+    text: <Trans i18nKey="wikiText">A wiki focused on the Blue Amazon</Trans>,
     active: true,
     image: "Wiki.png",
     imageAlt: "Ilustração de um Wiki",
