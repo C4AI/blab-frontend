22c637cec535298de1462b83310bedece79bae94
rodtav <rodrigotav25@gmail.com>
2022-04-25 11:50:21 -0300

    Initial implementation of Language Switch

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import { withTranslation } from "react-i18next";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 
-import Page404 from "./components/Page404.js"
+import Page404 from "./components/Page404.js";
 import {Routes, Route} from "react-router-dom";
+import LanguageSwitch from "./components/LanguageSwitch.js";
 
 const App = withTranslation()(() => {
 
   return (
     <div className="App">
       <CssBaseline />
-        <Routes>
-          <Route exact path="/" element={<Body />}>
-          </Route>
-          <Route path="*" element={<Page404 />}>
-          </Route>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Body />}>
+        </Route>
+        <Route path="*" element={<Page404 />}>
+        </Route>
+      </Routes>
+      <LanguageSwitch/>
       <Footer />
     </div>
   );
