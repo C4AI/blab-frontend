32e4dc7dc82e7b90277354e7b54f498ad3abe72b
rodtav <rodrigotav25@gmail.com>
2022-05-12 17:58:54 -0300

    Changed PopUp text and began improvements on PopUp element

--- src/components/Body/Services/Initial/Popup.js
+++ src/components/Body/Services/Initial/Popup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import { Box, Typography } from '@mui/material'
 import { Trans } from "react-i18next";
 
 /**
@@ -15,7 +15,27 @@ const Popup = ({ handleClose }) => {
     <div className="popup-box">
       <div className="box">
         <span className="close-icon" onClick={handleClose}>x</span>
-        <Trans i18nKey="popupText">Lorem Ipsum</Trans>
+        <Box
+          component="img"
+          sx={{
+            height: {
+              xs: 211,
+              md: 211*1.25,
+              lg: 212*1.5,
+            },
+            width: {
+              xs: 212,
+              md: 212*1.25,
+              lg: 212*1.5,
+            },
+            flex: 1,
+            verticalAlign: "text-top"
+          }}
+          src={require("../../../../images/Imagem_Texto_Introdutorio.png")}
+        />
+        <Typography>
+          <Trans i18nKey="popupText">Lorem Ipsum</Trans>
+        </Typography>
       </div>
     </div>
   );
