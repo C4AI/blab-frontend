17aa11e516807f5d65492b0f023d116599b6b26c
rsgrava <rsgrava@hotmail.com>
2022-06-16 19:47:20 -0300

    browser compatibility detection

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ import "./App.css";
 import Page404 from "./components/Page404.js";
 import {Routes, Route} from "react-router-dom";
 import LanguageSwitch from "./components/LanguageSwitch";
+import isValidBrowser from "./detectBrowser";
 
 const App = withTranslation()(() => {
-    console.log("teste");
     return (
-    <div className="App">
-      <CssBaseline />
-      <Routes>
-        <Route exact path="/" element={<Body initialLanguage={null}/>}/>
-        <Route path="*" element={<Page404 />}/>
-      </Routes>
-      <LanguageSwitch/>
-      <Footer />
-    </div>
+    isValidBrowser ?
+      <div className="App">
+        <CssBaseline />
+        <Routes>
+          <Route exact path="/" element={<Body initialLanguage={null}/>}/>
+          <Route path="*" element={<Page404 />}/>
+        </Routes>
+        <LanguageSwitch/>
+        <Footer />
+      </div>
+      :
+      <div className="invalidBrowser">A versão de seu navegador é insuficiente para carregar o site.
+           Por favor, atualize seu navegador e tente novamente.</div>
   );
 });
 
